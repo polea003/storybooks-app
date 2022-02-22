9bfa16be2bbdcfff7002b3073dc2030717d3a7f0
polea003 <polea003@fiu.edu>
2022-02-22 18:08:42 -0500

    login and logout

--- routes/auth.js
+++ routes/auth.js
@@ -13,5 +13,14 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     res.redirect('/dashboard')
 })
 
+// @desc logout user
+// @route GET /logout
+router.get('/logout', 
+(req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+
 
 module.exports = router
